c54080d6233e88bb941aa4444c4b5559cf3797f2
boxS0225 <167198715+Be-konjouyu@users.noreply.github.com>
2024-08-19 13:28:19 +0900

    あ

--- Source/GhostBurster/Private/Enemy/NormalEnemy.cpp
+++ Source/GhostBurster/Private/Enemy/NormalEnemy.cpp
@@ -78,7 +78,7 @@ void ANormalEnemy::Think()
 	switch (nowState)
 	{
 	case State::Stand:	//—§‚Á‚Ä‚¢‚é
-		if (MoveCount >= 60 * 5 * Gamefps / 60) { nowState = State::Attack; }
+		if (MoveCount >= 60 * 1 * Gamefps / 60) { nowState = State::Attack; }
 		if (Status.HP <= 0) { nowState = State::Die; }
 		break;
 
@@ -88,7 +88,7 @@ void ANormalEnemy::Think()
 		break;
 
 	case State::Attack:	//UŒ‚
-		if (MoveCount >= AttackUpToTime * Gamefps / 60) { nowState = State::Stand; }
+		if (MoveCount >= 60 * Gamefps / 60) { nowState = State::Stand; }
 		if (Status.HP <= 0) { nowState = State::Die; }
 		break;
 	}
@@ -108,7 +108,7 @@ void ANormalEnemy::Move()
 		break;
 
 	case State::Attack:	//UŒ‚
-		if (MoveCount == 15 * Gamefps / 60) //15‚Ì•”•ª‚ÍUŒ‚ƒ‚[ƒVƒ‡ƒ“‚É‡‚í‚¹‚Ä•ÏX‚·‚é
+		if (MoveCount == AttackUpToTime * Gamefps / 60) //15‚Ì•”•ª‚ÍUŒ‚ƒ‚[ƒVƒ‡ƒ“‚É‡‚í‚¹‚Ä•ÏX‚·‚é
 		{
 			//UŒ‚‚·‚é
 			UKismetSystemLibrary::PrintString(this, TEXT("WhiteEnemy Attack!"), true, true, FColor::White, 2.f, TEXT("None"));
